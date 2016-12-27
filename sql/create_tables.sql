f145d4e0cdeec4683f6aae51b1cfc1cdb9b199f8
Janne Vuorela <janne.vuorela@helsinki.fi>
2016-12-27 23:58:23 +0200

    asd

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,22 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Kayttaja(
+  ID SERIAL PRIMARY KEY, 
+  Nimi varchar(120) NOT NULL,
+  Salasana varchar(120) NOT NULL
+);
+
+CREATE TABLE Resepti(
+  ID SERIAL PRIMARY KEY,
+  Nimi varchar(120) NOT NULL,
+  Kayttaja_ID INTEGER REFERENCES Kayttaja(ID),
+  Hyvaksytty boolean DEFAULT FALSE
+);
+
+CREATE TABLE Ainesosa(
+  ID SERIAL PRIMARY KEY,
+  Nimi varchar(120) NOT NULL,
+);
+
+CREATE TABLE Ainesosaliitos(
+  Resepti_ID INTEGER REFERENCES Resepti(ID),
+  Ainesosa INTEGER REFERENCES Ainesosa(ID),
+);
