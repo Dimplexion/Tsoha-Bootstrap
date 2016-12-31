fe321d8fd63a7842dafe14825558527fa4869f7c
Janne Vuorela <janne.vuorela@helsinki.fi>
2016-12-31 15:34:53 +0200

    sql:
    [*] Uudelleen nimetty DrinkRecipe taulun User_ID Owner_ID:ksi.

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,23 +1,4 @@
 /*
-CREATE TABLE Kayttaja(
-  ID SERIAL PRIMARY KEY, 
-  Nimi varchar(120) NOT NULL,
-  Salasana varchar(120) NOT NULL,
-  Yllapitaja boolean DEFAULT FALSE
-);
-
-CREATE TABLE Resepti(
-  ID SERIAL PRIMARY KEY,
-  Nimi varchar(120) NOT NULL,
-  Kayttaja_ID INTEGER REFERENCES Kayttaja(ID),
-  Hyvaksytty boolean DEFAULT FALSE
-);
-
-CREATE TABLE Ainesosa(
-  ID SERIAL PRIMARY KEY,
-  Nimi varchar(120) NOT NULL
-);
-
 CREATE TABLE Ainesosaliitos(
   Resepti_ID INTEGER REFERENCES Resepti(ID),
   Ainesosa_ID INTEGER REFERENCES Ainesosa(ID)
@@ -36,7 +17,7 @@ CREATE TABLE UserAccount(
 CREATE TABLE DrinkRecipe(
   ID SERIAL PRIMARY KEY,
   Name varchar(120) NOT NULL,
-  User_ID INTEGER REFERENCES UserAccount(ID),
+  Owner_ID INTEGER REFERENCES UserAccount(ID),
   Approved boolean DEFAULT FALSE
 );
 
